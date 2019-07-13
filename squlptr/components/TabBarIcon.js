071ff195be697923688f96c15ad8d12802af157c
Tobi Adeyemi <oadeyemi@teamapt.com>
2019-07-13 09:36:22 +0100

    Update bottom Navigation Icon

--- squlptr/components/TabBarIcon.js
+++ squlptr/components/TabBarIcon.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-
+import { Feather, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 import Colors from '../constants/Colors';
 import { Home } from '../assets/icons';
 
 export default function TabBarIcon(props) {
   return (
-    <Ionicons
-      name={props.name}
-      size={26}
-      style={{ marginBottom: -3 }}
-      color={props.focused ? Colors.squlptr : Colors.tabIconDefault}
-    />
-    // <View>
-    //   <Home />
-    // </View>
+    <>
+      {props.name === 'dots-horizontal' ? (
+        <MaterialCommunityIcons
+          name={props.name}
+          size={26}
+          style={{ marginBottom: -3 }}
+          color={props.focused ? Colors.squlptr : Colors.tabIconDefault}
+        />
+      ) : (
+        <Feather
+          name={props.name}
+          size={26}
+          style={{ marginBottom: -3 }}
+          color={props.focused ? Colors.squlptr : Colors.tabIconDefault}
+        />
+      )}
+    </>
   );
 }
